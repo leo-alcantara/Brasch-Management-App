f72e6f9adeaf7899b1499539c48cff20b4ca6cfd
leoodindealcantara <leoalcantara@icloud.com>
2021-09-24 16:08:40 +0200

    Brasch Management Managing System

---
+++ src/main/java/the/bug/tech/brasch_management_system/model/Company.java
@@ -0,0 +1,16 @@
+package the.bug.tech.brasch_management_system.model;
+
+import java.util.List;
+
+public class Company {
+
+    private int companyId;
+    private String companyName;
+    private String companyAddress;
+    private String companyPhoneNumber;
+    private List<ContactPerson> contactPersonList;
+    private List<Project> projectsList;
+
+
+
+}
